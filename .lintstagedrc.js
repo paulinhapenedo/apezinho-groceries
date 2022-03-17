7b037278391f5c82cbecddacf455a3daace57ab9
Paula Penedo <paulinhapenedo@gmail.com>
2022-03-16 21:38:08 -0300

    chore: move yarn tsc into the same lint-staged rule as lint and prettier

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
-
-  // Lint then format TypeScript and JavaScript files
+  // Type check TypeScript files, lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx)': (filenames) => [
+    'yarn tsc --noEmit',
     `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`,
     `yarn prettier --write ${filenames.join(' ')}`,
   ],
